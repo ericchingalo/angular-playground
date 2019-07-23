62627cb2b68431d2306463420fd5a1ab90545797
chingaloEric <echingalo@gmail.com>
2019-07-23 10:12:14 +0300

    observables lecture done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +9,29 @@ import { Observable } from 'rxjs';
 export class HomeComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const myObserverble = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        console.log('First package');
+      }, 2000);
+      setTimeout(() => {
+        console.log('Second package');
+      }, 4000);
+      setTimeout(() => {
+        observer.complete();
+      }, 5000);
+    });
+
+    myObserverble.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('COMPLETED');
+      }
+    );
+  }
 }
